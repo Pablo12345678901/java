386f7402b6dfc090ab0813d4d1b9c8c401fab89d
Pablo ZAMORA <pablo.zamora@outlook.fr>
2023-06-09 17:55:03 +0200

    Fin des servlets, JSP et JavaBean pour créer des pages web dynamiques

--- zb_28_programmation_java_cote_serveur_servlets_et_jsp/voirrepertoiretomcat_version_libexec_webapps_ROOT_testperso.java
+++ zb_28_programmation_java_cote_serveur_servlets_et_jsp/voirrepertoiretomcat_version_libexec_webapps_ROOT_testperso.java
@@ -4,4 +4,5 @@ public class voirrepertoiretomcat_version_libexec_webapps_ROOT_testperso {
     // les tests de servets ont été fait dans le répertoire ci-dessous (répertoire push sur Git)
     // accessible via localhost (avec Tomcat) : 
     // MDP admin admin '/usr/local/Cellar/tomcat/10.1.9/libexec/webapps/ROOT/testperso'
+    //
 }
